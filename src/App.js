cd48ec5095d78b445df1e8086e66f97117613752
kanekyo1234 <kanekyohunter.0314@softbank.ne.jp>
2021-05-06 23:33:49 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+import data from "./data.json";
+import { ResponsiveNetwork } from "@nivo/network";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {console.log(data)}
+      <ResponsiveNetwork
+        data={data}
+        margin={{ top: 100, right: 100, bottom: 100, left: 100 }}
+        repulsivity={100}
+        iterations={60}
+        nodeColor={function (e) {
+          return e.color;
+        }}
+        nodeBorderWidth={1}
+        nodeBorderColor={{ from: "color", modifiers: [["darker", 0.8]] }}
+        linkThickness={function (e) {
+          return 2 * (2 - e.source.depth);
+        }}
+        motionStiffness={160}
+        motionDamping={12}
+      />
+      <p>HJNJN</p>
     </div>
   );
 }
-
 export default App;
