759c9c94e714d5586dc36f3f52f6e0a94beee196
FugaTakata <maple1213goo@gmail.com>
2021-05-18 15:20:09 +0900

    ꉂ (๑¯ਊ¯)σ л̵ʱªʱªʱª [笑]

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@
 import "./App.css";
 import networkData from "./networkData.json";
 import bumpData from "./genres_data.json";
-import ResponsiveNetwork from "./responsiveNetwork.js";
-import ResponsiveBump from "./responsiveBump.js";
+import ResponsiveNetwork from "./components/responsiveNetwork.js";
+import ResponsiveBump from "./components/responsiveBump.js";
 
 function App() {
   return (
